73cee2037613224b435c4d1f3108abbc051adaf5
imran56-git <kaka7in1@gmail.com>
2025-05-17 02:52:23 +0530

    Update chat_service.dart

--- lib/services/chat_service.dart
+++ lib/services/chat_service.dart
@@ -3,7 +3,6 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 class ChatService {
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
 
-  // আপডেট অনলাইন স্ট্যাটাস
   Future<void> updateOnlineStatus(String userId, bool isOnline) async {
     await _firestore.collection('users').doc(userId).update({
       'status': isOnline ? 'Online' : 'Last seen',
@@ -11,7 +10,6 @@ class ChatService {
     });
   }
 
-  // আপডেট টাইপিং স্ট্যাটাস
   Future<void> updateTypingStatus(String chatId, String userId, bool isTyping) async {
     await _firestore.collection('chats').doc(chatId).collection('typing').doc(userId).set({
       'typing': isTyping,
@@ -19,7 +17,6 @@ class ChatService {
     });
   }
 
-  // স্ট্রিমে অনলাইন বা লাস্ট সিন শুনে নেওয়া
   Stream<Map<String, dynamic>> getUserStatusStream(String userId) {
     return _firestore.collection('users').doc(userId).snapshots().map((doc) {
       final data = doc.data();
@@ -30,7 +27,6 @@ class ChatService {
     });
   }
 
-  // স্ট্রিমে টাইপিং স্ট্যাটাস শুনে নেওয়া
   Stream<bool> getTypingStatusStream(String chatId, String userId) {
     return _firestore
         .collection('chats')
