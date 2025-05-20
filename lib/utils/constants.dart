e4c0a31e2a5f86264a5697a5f5ec2475ffb11a0c
imran56-git <kaka7in1@gmail.com>
2025-05-20 19:02:19 +0530

    Update constants.dart

---
+++ lib/utils/constants.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class AppConstants {
+  // App Information
+  static const String appName = "Find Your Best Teacher Today";
+  static const String apiBaseUrl = "https://your-api-url.com"; 
+
+  // Common Colors
+  static const Color primaryColor = Colors.blue;
+  static const Color backgroundColor = Colors.white;
+
+  // Layout constants
+  static const double defaultPadding = 16.0;
+  static const double defaultRadius = 12.0;
+
+  // Common Messages
+  static const String errorMessage = "Something went wrong";
+  static const String successMessage = "Update successful";
+}
