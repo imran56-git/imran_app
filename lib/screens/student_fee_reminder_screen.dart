09b25f94239c81b5d0a5ea6e435ad392cd91984c
imran56-git <kaka7in1@gmail.com>
2025-05-24 19:59:20 +0530

    Create student_fee_reminder_screen.dart

---
+++ lib/screens/student_fee_reminder_screen.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class StudentFeeReminderScreen extends StatelessWidget {
+  final String teacherId;
+
+  StudentFeeReminderScreen({required this.teacherId});
+
+  @override
+  Widget build(BuildContext context) {
+    final studentsRef = FirebaseFirestore.instance
+        .collection('teachers')
+        .doc(teacherId)
+        .collection('students');
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Fee Reminders"),
+      ),
+      body: StreamBuilder<QuerySnapshot>(
+        stream: studentsRef.snapshots(),
+        builder: (context, snapshot) {
+          if (!snapshot.hasData) return CircularProgressIndicator();
+
+          final students = snapshot.data!.docs;
+
+          return ListView.builder(
+            itemCount: students.length,
+            itemBuilder: (context, index) {
+              final student = students[index];
+              return ListTile(
+                title: Text(student['name']),
+                subtitle: Text("Amount: ${student['feeAmount']} | Day: ${student['reminderDay']}"),
+                trailing: IconButton(
+                  icon: Icon(Icons.edit),
+                  onPressed: () {
+                    showDialog(
+                      context: context,
+                      builder: (_) => SetReminderDialog(
+                        teacherId: teacherId,
+                        studentId: student.id,
+                        existingData: student,
+                      ),
+                    );
+                  },
+                ),
+              );
+            },
+          );
+        },
+      ),
+    );
+  }
+}
