6eda545361b54b7dacff6ccfd08a2820a0a8c423
imran56-git <kaka7in1@gmail.com>
2025-05-27 14:04:51 +0530

    Update student_home_screen.dart

---
+++ lib/screens/student_home_screen.dart
@@ -0,0 +1,63 @@
+// File: student_home_screen.dart
+
+import 'package:flutter/material.dart';
+import 'package:find_your_best_teacher_today/screens/student_profile_screen.dart';
+import 'package:find_your_best_teacher_today/screens/teacher_search_screen.dart';
+import 'package:find_your_best_teacher_today/screens/chat/chat_home_screen.dart';
+
+class StudentHomeScreen extends StatefulWidget {
+  const StudentHomeScreen({super.key});
+
+  @override
+  State<StudentHomeScreen> createState() => _StudentHomeScreenState();
+}
+
+class _StudentHomeScreenState extends State<StudentHomeScreen> {
+  int _selectedIndex = 0;
+
+  final List<Widget> _screens = [
+    const ChatHomeScreen(),
+    const TeacherSearchScreen(),
+    const StudentProfileScreen(),
+  ];
+
+  final List<String> _titles = [
+    "Chats",
+    "Search Teachers",
+    "My Profile",
+  ];
+
+  void _onTabTapped(int index) {
+    setState(() => _selectedIndex = index);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(_titles[_selectedIndex]),
+        centerTitle: true,
+      ),
+      body: _screens[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        onTap: _onTabTapped,
+        selectedItemColor: Colors.deepPurple,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.chat_bubble_outline),
+            label: 'Chats',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search),
+            label: 'Search',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person_outline),
+            label: 'Profile',
+          ),
+        ],
+      ),
+    );
+  }
+}
