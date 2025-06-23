aed9774811ac80647e3e8cd0db187a1c9beb67ee
imran56-git <kaka7in1@gmail.com>
2025-06-23 13:53:54 +0530

    Update badge_widget.dart

--- lib/widgets/badge_widget.dart
+++ lib/widgets/badge_widget.dart
@@ -11,22 +11,23 @@ class BadgeWidget extends StatelessWidget {
     String imagePath = '';
     switch (badgeCode) {
       case 'K':
-        imagePath = 'assets/badges/k_badge.png';
+        imagePath = 'assets/images/badges/k_badge.png';
         break;
       case 'B':
-        imagePath = 'assets/badges/b_badge.png';
+        imagePath = 'assets/images/badges/b_badge.png';
         break;
       case 'M':
-        imagePath = 'assets/badges/m_badge.png';
+        imagePath = 'assets/images/badges/m_badge.png';
         break;
       default:
-        return const SizedBox.shrink();
+        return const SizedBox.shrink(); // যদি ব্যাজ কোড না মেলে
     }
 
     return Image.asset(
       imagePath,
       width: size,
       height: size,
+      fit: BoxFit.contain,
     );
   }
 }
