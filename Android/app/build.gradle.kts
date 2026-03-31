f2ef41074553b7d9b85f2073e0d2c1cdb7dacacb
imran56-git <kaka7in1@gmail.com>
2026-03-31 16:19:13 +0530

    Update build.gradle.kts

--- Android/app/build.gradle.kts
+++ Android/app/build.gradle.kts
@@ -1,47 +1,55 @@
-{
-  "project_info": {
-    "project_number": "575213013835",
-    "project_id": "find-your-best-teacher-today",
-    "storage_bucket": "find-your-best-teacher-today.firebasestorage.app"
-  },
-  "client": [
-    {
-      "client_info": {
-        "mobilesdk_app_id": "1:575213013835:android:d1b593868111d322e076ce",
-        "android_client_info": {
-          "package_name": "com.imran.findyourbestteacher"
-        }
-      },
-      "oauth_client": [
-        {
-          "client_id": "575213013835-6vmhuc5tc13il0fla1ifru2d3a0o7ups.apps.googleusercontent.com",
-          "client_type": 3
-        },
-        {
-          "client_id": "575213013835-7ram8ko8ivtvggg38oi88alkrdmqnpu5.apps.googleusercontent.com",
-          "client_type": 1,
-          "android_info": {
-            "package_name": "com.imran.findyourbestteacher",
-            "certificate_hash": "843ddf1257dc5b2e8fc1494d25bcd5380f423206"
-          }
-        }
-      ],
-      "api_key": [
-        {
-          "current_key": "AIzaSyDmQiaw36SnsqbC8UlOord7krWJElPtXZA"
-        }
-      ],
-      "services": {
-        "appinvite_service": {
-          "other_platform_oauth_client": [
-            {
-              "client_id": "575213013835-6vmhuc5tc13il0fla1ifru2d3a0o7ups.apps.googleusercontent.com",
-              "client_type": 3
-            }
-          ]
-        }
-      }
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+    id("com.google.gms.google-services")
+}
+
+android {
+    namespace = "com.imran.findyourbestteacher"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.imran.findyourbestteacher"
+        minSdk = 21
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
-  ],
-  "configuration_version": "1"
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+}
+
+dependencies {
+    implementation(platform("com.google.firebase:firebase-bom:32.7.3"))
+
+    implementation("com.google.firebase:firebase-auth")
+    implementation("com.google.firebase:firebase-firestore")
+    implementation("com.google.firebase:firebase-messaging")
+    implementation("com.google.firebase:firebase-analytics")
+
+    implementation("androidx.work:work-runtime-ktx:2.9.0")
+    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.9.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
