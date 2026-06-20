3c17135604695978dc9388c982972cf08c6222e6
imran56-git <kaka7in1@gmail.com>
2026-06-20 08:44:11 +0530

    Update splash_screen.dart

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -6,72 +6,79 @@ import '../widgets/shiny_loader.dart';
 class SplashScreen extends StatefulWidget {
   const SplashScreen({Key? key}) : super(key: key);
 
-    @override
-      State<SplashScreen> createState() => _SplashScreenState();
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> with TickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _rotationAnimation;
+  late Animation<double> _scaleAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = AnimationController(
+      duration: const Duration(milliseconds: 1500),
+      vsync: this,
+    );
+
+    // Rotates the logo half turn (-0.5 represents 180 degrees)
+    _rotationAnimation = Tween<double>(begin: -0.5, end: 0.0).animate(
+      CurvedAnimation(parent: _controller, curve: Curves.easeOutBack),
+    );
+
+    // Scales the logo from 0.5 to 1.0
+    _scaleAnimation = Tween<double>(begin: 0.5, end: 1.0).animate(
+      CurvedAnimation(parent: _controller, curve: Curves.easeOut),
+    );
+
+    _controller.forward();
+
+    Timer(const Duration(seconds: 3), () {
+      if (mounted) {
+        Navigator.pushReplacement(
+          context,
+          PageRouteBuilder(
+            pageBuilder: (context, animation, secondaryAnimation) => const WelcomeScreen(),
+            transitionsBuilder: (context, animation, secondaryAnimation, child) {
+              return FadeTransition(opacity: animation, child: child);
+            },
+            transitionDuration: const Duration(milliseconds: 800),
+          ),
+        );
       }
+    });
+  }
 
-      class _SplashScreenState extends State<SplashScreen>
-          with TickerProviderStateMixin {
-            late AnimationController _bounceController;
-              late Animation<double> _bounceAnimation;
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
 
-                @override
-                  void initState() {
-                      super.initState();
-
-                          _bounceController = AnimationController(
-                                  duration: const Duration(milliseconds: 1200), vsync: this);
-                                      _bounceAnimation =
-                                              CurvedAnimation(parent: _bounceController, curve: Curves.elasticOut);
-
-                                                  _bounceController.forward();
-
-                                                      Timer(const Duration(seconds: 3), () {
-                                                            if (mounted) {
-                                                                    Navigator.pushReplacement(
-                                                                              context,
-                                                                                        PageRouteBuilder(
-                                                                                                    pageBuilder: (context, animation, secondaryAnimation) =>
-                                                                                                                    const WelcomeScreen(),
-                                                                                                                                transitionsBuilder: (context, animation, secondaryAnimation, child) {
-                                                                                                                                              return FadeTransition(
-                                                                                                                                                              opacity: animation,
-                                                                                                                                                                              child: child,
-                                                                                                                                                                                            );
-                                                                                                                                                                                                        },
-                                                                                                                                                                                                                    transitionDuration: const Duration(milliseconds: 800),
-                                                                                                                                                                                                                              ),
-                                                                                                                                                                                                                                      );
-                                                                                                                                                                                                                                            }
-                                                                                                                                                                                                                                                });
-                                                                                                                                                                                                                                                  }
-
-                                                                                                                                                                                                                                                    @override
-                                                                                                                                                                                                                                                      void dispose() {
-                                                                                                                                                                                                                                                          _bounceController.dispose();
-                                                                                                                                                                                                                                                              super.dispose();
-                                                                                                                                                                                                                                                                }
-
-                                                                                                                                                                                                                                                                  @override
-                                                                                                                                                                                                                                                                    Widget build(BuildContext context) {
-                                                                                                                                                                                                                                                                        return Scaffold(
-                                                                                                                                                                                                                                                                              backgroundColor: Colors.black,
-                                                                                                                                                                                                                                                                                    body: Center(
-                                                                                                                                                                                                                                                                                            child: ScaleTransition(
-                                                                                                                                                                                                                                                                                                      scale: _bounceAnimation,
-                                                                                                                                                                                                                                                                                                                child: CustomShinyLoading(
-                                                                                                                                                                                                                                                                                                                            child: const Text(
-                                                                                                                                                                                                                                                                                                                                          'IM',
-                                                                                                                                                                                                                                                                                                                                                        style: TextStyle(
-                                                                                                                                                                                                                                                                                                                                                                        fontSize: 90,
-                                                                                                                                                                                                                                                                                                                                                                                        fontWeight: FontWeight.bold,
-                                                                                                                                                                                                                                                                                                                                                                                                        letterSpacing: 2.0,
-                                                                                                                                                                                                                                                                                                                                                                                                                      ),
-                                                                                                                                                                                                                                                                                                                                                                                                                                  ),
-                                                                                                                                                                                                                                                                                                                                                                                                                                            ),
-                                                                                                                                                                                                                                                                                                                                                                                                                                                    ),
-                                                                                                                                                                                                                                                                                                                                                                                                                                                          ),
-                                                                                                                                                                                                                                                                                                                                                                                                                                                              );
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                }
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                }
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Center(
+        child: RotationTransition(
+          turns: _rotationAnimation,
+          child: ScaleTransition(
+            scale: _scaleAnimation,
+            child: CustomShinyLoading(
+              child: Image.asset(
+                'assets/images/logo.png',
+                width: 200,
+                height: 200,
+                fit: BoxFit.contain,
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+                                                                                                                                                                                                                                                                                                                                                                                                    
