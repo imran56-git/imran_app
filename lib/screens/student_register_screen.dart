3d502f12b5eb89bcbc2926cbc499e6f63aa9ab90
imran56-git <kaka7in1@gmail.com>
2026-07-05 13:44:57 +0530

    Update student_register_screen.dart

--- lib/screens/student_register_screen.dart
+++ lib/screens/student_register_screen.dart
@@ -3,6 +3,7 @@ import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:url_launcher/url_launcher.dart';
@@ -285,6 +286,8 @@ await FirebaseFirestore.instance
   if (mounted) {
     setState(() => _isLoading = false);
   }
+
+}
 
 }
 
