70bafa7c4c09b130bd04654556fe951bde62b1d4
imran56-git <kaka7in1@gmail.com>
2026-07-11 16:50:42 +0530

    Create chat_colors.dart

---
+++ lib/utils/chat_colors.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class ChatColors {
+  static const Color primaryApp = Color(0xFF00A884);
+  static const Color primaryDark = Color(0xFF005E4C);
+  
+  static const Color bgLight = Color(0xFFECE5DD);
+  static const Color bgDark = Color(0xFF111B21);
+
+  static const Color appBarLight = Color(0xFF008069);
+  static const Color appBarDark = Color(0xFF202C33);
+
+  static const Color bubbleSenderLight = Color(0xFFD9FDD3);
+  static const Color bubbleSenderDark = Color(0xFF005C4B);
+
+  static const Color bubbleReceiverLight = Color(0xFFFFFFFF);
+  static const Color bubbleReceiverDark = Color(0xFF202C33);
+
+  static const Color textMainLight = Color(0xFF111B21);
+  static const Color textMainDark = Color(0xFFE9EDEF);
+
+  static const Color textSecondaryLight = Color(0xFF667781);
+  static const Color textSecondaryDark = Color(0xFF8696A0);
+
+  static const Color iconLight = Color(0xFF54656F);
+  static const Color iconDark = Color(0xFFAEBAC1);
+
+  static const Color tickUnsent = Color(0xFF8696A0);
+  static const Color tickDelivered = Color(0xFF8696A0);
+  static const Color tickSeen = Color(0xFF53BDEB);
+}
