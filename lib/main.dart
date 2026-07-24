779b3eafcf48fb6964ee07411dc884e653d79e21
imran56-git <kaka7in1@gmail.com>
2026-07-24 23:06:35 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:find_your_best_teacher_today/ratings_badges_system.dart';
 
 import 'firebase_options.dart'; 
 import 'routes/app_routes.dart'; 
