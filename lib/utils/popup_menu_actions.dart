9780e05c5b4e5e54aefd4c9eea49cf0c17726b12
imran56-git <kaka7in1@gmail.com>
2026-07-29 17:47:57 +0530

    Update popup_menu_actions.dart

--- lib/utils/popup_menu_actions.dart
+++ lib/utils/popup_menu_actions.dart
@@ -5,3 +5,32 @@ enum ChatMenuAction {
   clearChat,
   deleteConversation,
 }
+
+extension ChatMenuActionExtension on ChatMenuAction {
+  String get title {
+    switch (this) {
+      case ChatMenuAction.viewProfile:
+        return 'View Profile';
+      case ChatMenuAction.blockUser:
+        return 'Block User';
+      case ChatMenuAction.reportUser:
+        return 'Report User';
+      case ChatMenuAction.clearChat:
+        return 'Clear Chat';
+      case ChatMenuAction.deleteConversation:
+        return 'Delete Conversation';
+    }
+  }
+
+  bool get isDestructive {
+    switch (this) {
+      case ChatMenuAction.blockUser:
+      case ChatMenuAction.clearChat:
+      case ChatMenuAction.deleteConversation:
+        return true;
+      case ChatMenuAction.viewProfile:
+      case ChatMenuAction.reportUser:
+        return false;
+    }
+  }
+}
